30aac0324aa15fc2c5628e853093167c65569a5c
Nikita <main.acr0matic@gmail.com>
2021-02-03 21:13:33 +0300

    feat: доработаны формы

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -22,3 +22,13 @@ const modalParams = {
 };
 
 MicroModal.init(modalParams);
+
+const forms = document.querySelectorAll('form');
+forms.forEach(form => {
+  phone = form.querySelector('input[type=tel]');
+
+  IMask(phone, {
+    mask: '+{7} (000) 000-00-00',
+    prepare: (appended, masked) => (appended === '8' && masked.value === '') ? '' : appended
+  });
+});
