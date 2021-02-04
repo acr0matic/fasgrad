f9164598a835205090ba226ae3205239ed1bba03
Nikita <main.acr0matic@gmail.com>
2021-02-04 21:43:32 +0300

    feat: поправлена / доработана верстка и адаптив (< 1200px )

--- src/scripts/service.js
+++ src/scripts/service.js
@@ -13,8 +13,15 @@ const Service = (() => {
       serviceCards.forEach(card => {
         const icon = card.querySelector('.service-card__icon');
         const tooltip = card.querySelector('.service-card__tooltip');
+        const tooltipBounding = tooltip.getBoundingClientRect();
 
-        icon.addEventListener('mouseover', () => tooltip.classList.add('tooltip--visible'));
+        icon.addEventListener('mouseover', () => {
+          if (tooltipBounding.right > (window.innerWidth || document.documentElement.clientWidth)) {
+            tooltip.classList.add('service-card__tooltip--left');
+          }
+
+          tooltip.classList.add('tooltip--visible');
+        });
         icon.addEventListener('mouseout', () => tooltip.classList.remove('tooltip--visible'));
       });
     }
