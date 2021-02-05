e6643f98d59d94471cb00a14c494a868566f24fe
Nikita <main.acr0matic@gmail.com>
2021-02-06 00:43:51 +0300

    feat: проставлены точки на карте

--- src/scripts/yMap.js
+++ src/scripts/yMap.js
@@ -2,9 +2,36 @@ ymaps.ready(function () {
   container = document.getElementById('yandexMap');
 
   var map = new ymaps.Map(container, {
-    center: [44.558367, 38.095674],
-    zoom: 15
+    center: [56.331927, 44.023225],
+    zoom: 8
   });
 
   map.behaviors.disable('scrollZoom');
+
+  fetch('../src/data/map.json')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      for (const key in data) {
+        const element = data[key];
+
+        let iconSize = [50, 40];
+
+        if (window.matchMedia("(max-width: 768px)").matches) iconSize = [35, 35];
+
+        var place = new ymaps.Placemark(
+          element.coordinates, {
+          balloonContentBody: '',
+        },
+          {
+            iconImageHref: '../src/img/icons/geo--orange.svg',
+            iconImageSize: iconSize,
+            iconLayout: 'default#image',
+          }
+        );
+
+        map.geoObjects.add(place);
+      }
+    });
 });
