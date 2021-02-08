27dee085dc1a4eaaac3375256632aab54b1093f3
Nikita <main.acr0matic@gmail.com>
2021-02-08 16:58:00 +0300

    feat: доработано мобильное меню

--- src/scripts/mobile-menu.js
+++ src/scripts/mobile-menu.js
@@ -1,8 +1,14 @@
 const mobileMenu = document.getElementById('mobile-menu');
 const mobileMenuButton = document.getElementById('mobile-button');
+const mobileMenuClose = mobileMenu.querySelector('.mobile-menu__close');
 const mobileMenuItems = mobileMenu.querySelectorAll('.mobile-menu__item');
 
 mobileMenuButton.addEventListener('click', () => {
+  mobileMenu.classList.toggle('mobile-menu--open');
+  mobileMenuButton.classList.toggle('is-active');
+});
+
+mobileMenuClose.addEventListener('touchmove', () => {
   mobileMenu.classList.toggle('mobile-menu--open');
   mobileMenuButton.classList.toggle('is-active');
 });
